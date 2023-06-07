82107ccff82fc6766f05ee69bab4d8ec4302eb44
jenihun <ghkddnjs1014@naver.com>
2023-06-08 01:53:06 +0900

    error삭제

--- src/Authentication.js
+++ src/Authentication.js
@@ -1,9 +1,9 @@
 import {GoogleAuthProvider, auth, firebaseConfig,signInWithPopup, initializeApp, onAuthStateChanged,signOut} from './firebase';
-import { google } from "googleapis";
+// import { google } from "googleapis";
 
 
 const googleClientId = "121079642070-ba0osda37t6s7op574peuvqrdjpogu99.apps.googleusercontent.com";
-const client = new google.auth.OAuth2(googleClientId);
+// const client = new google.auth.OAuth2(googleClientId);
 
 
 //로그인
@@ -21,20 +21,20 @@ export const login = async () => {
         console.log('user :', result.user)
         console.log('tocken :', result.credential.accessToken)
 
-        const googleAccessToken = result.credential.accessToken;
-        client.setCredentials({ access_token: googleAccessToken });
+        // const googleAccessToken = result.credential.accessToken;
+        // client.setCredentials({ access_token: googleAccessToken });
 
-        google.people({ version: "v1", auth: client }).people.get({
-          resourceName: "people/me",
-          personFields: "names,genders",
-        }, (data) => {
-            // 사용자의 나이와 성별 추출
-            const age = data.names[0].metadata.source.age;
-            const gender = data.genders[0].value;
+        // google.people({ version: "v1", auth: client }).people.get({
+        //   resourceName: "people/me",
+        //   personFields: "names,genders",
+        // }, (data) => {
+        //     // 사용자의 나이와 성별 추출
+        //     const age = data.names[0].metadata.source.age;
+        //     const gender = data.genders[0].value;
             
-            console.log("나이:", age);
-            console.log("성별:", gender);
-        });
+        //     console.log("나이:", age);
+        //     console.log("성별:", gender);
+        // });
     
         
 
