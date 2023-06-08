156e4eddf4e9d280235805e1b08dfbf942d9936d
jenihun <ghkddnjs1014@naver.com>
2023-06-08 15:55:17 +0900

    확장자 변환 및 정리

--- src/FPS.jsx
+++ src/FPS.jsx
@@ -1,8 +1,6 @@
 import {React, useEffect, useState} from "react"; //React를 임포트한다. React컴포넌트를 정의하기 위해 필요한 도구
-import {createNode, printAllNodes, deleteNode, nodesCollection} from "./dataFunctions";  //노드 관리에 관련된 함수
-import { login, logout } from "./Authentication";
-import { useNavigate } from 'react-router-dom';
-import {getDocs, collection, db} from './firebase';
+import {createNode, printAllNodes, deleteNode, createPlan, clearAllNodes} from "./dataFunctions";  //노드 관리에 관련된 함수
+import {MainSpace, login} from "./Authentication";
 
 /*
 return 문 안에는 단일 요소만 반환해야한다. 부모 요소에 포함된 여러 개의 자식 요소들로 구성
@@ -11,60 +9,37 @@ return 문 안에는 단일 요소만 반환해야한다. 부모 요소에 포�
 */
 
 const FPS = () => {
-  const navigate = useNavigate();
-  
-  const handlespace = () => {
-    navigate('/main');
-  };
-
-  const logoutHandler = () => {
-    logout();
-    handlespace();
-  }
 
   const [nodes, setNodes] = useState([]);
 
-  const printAllNodes = async () => {
-    try {
-      const querySnapshot = await getDocs(nodesCollection);
-      const sortedNodes = querySnapshot.docs
-        .map((doc) => {
-          return {...doc.data(), id: doc.id}
-        })
-        .sort((a, b) => a.title.localeCompare(b.title));
-
-      // sortedNodes.forEach((nodeData) => {
-      //   console.log('노드 정보:', nodeData);
-      // });
-
-      console.log('full node :', sortedNodes)
-
-      setNodes(sortedNodes);
-    } catch (error) {
-      console.error('노드 정보 가져오기에 실패하였습니다.', error);
-    }
-  };
-
-  const clearAllNodes = () => {
-    setNodes([]);
-  }
 
   useEffect(() => {
-    console.log('nodes', nodes)
-  }, [nodes])
-  
-  useEffect(() => {
-    printAllNodes();
+    printAllNodes(setNodes);
   }, [])
+
+  const handleCreateNode = async () => {
+    createNode(); // 노드 생성
+    printAllNodes(setNodes);
+  };
 
   return (
     <div>
+      <div>
+        <h1>계획 생성</h1>
+        <div>
+          <input type = "text" name="planTitle" placeholder="제목"/>
+          <button onClick={() => {createPlan();}}>계획 생성</button>
+        </div>
+
+      </div>
+
+
       <div>
         <h1>노드 생성</h1>
         <div className="create-node-form">
         <input type="text" name="title" placeholder="제목"/>
         <textarea name="content" placeholder="내용"></textarea>
-        <button onClick={() => {createNode();printAllNodes();}}>노드 생성</button>
+        <button onClick={handleCreateNode}>노드 생성</button>
         </div>
         <div>
         <h1>노드 삭제</h1>
@@ -86,21 +61,20 @@ const FPS = () => {
         <div>
         <h1>로그인 기능</h1>
         <button onClick={login}>로그인</button>
-        <button onClick={logoutHandler}>로그아웃</button>
-        <button onClick={handlespace}>main으로 이동</button>
+        <button onClick={MainSpace}>main으로 이동</button>
         </div>
 
         <div>
         <h1>노드 정보</h1>
-        <button onClick={printAllNodes}>노드 내용 출력</button>
-        <button onClick={clearAllNodes}>노드 내용 clear</button>
+        <button onClick={() =>{printAllNodes(setNodes)}}>노드 내용 출력</button>
+        <button onClick={() => {clearAllNodes(setNodes)}}>노드 내용 clear</button>
+        </div>
 
+
+    
         
-          
-        
-
-        <hr/>
-
+        <div style={{'padding' : '100px', 'border' : ' 5px solid black'}}></div>
+        <div>
         <div style={{'height':'500px', 'backgroundColor':'cyan'}}>
           <div style={{'border':'3px solid black', 'display':'flex', 'gap':'30px'}}>
 
@@ -109,7 +83,7 @@ const FPS = () => {
             {nodes.map((node, index) => (
               <div key={index} style={{'padding':'10px', 'height':"100px", 'width':'100px', 'backgroundColor':'white', 'border':'1px solid black'}}> 
                 <div  id={node.id}  style={{'border': '1px black solid'}}>
-                  <h2 onClick={(e) => {deleteNode(e.target.parentElement.id); printAllNodes();}} style={{"backgroundColor":"red"}} >{node.title}</h2>
+                  <h2 onClick={(e) => {deleteNode(e.target.parentElement.id); printAllNodes(setNodes);}} style={{"backgroundColor":"red"}} >{node.title}</h2>
                   <p>{node.content}</p>
                 </div>
               </div>
@@ -119,6 +93,7 @@ const FPS = () => {
         </div>
       </div>
     </div>
+
     </div>
 );
 };
---
+++ src/dataFunctions.jsx
@@ -0,0 +1,112 @@
+import { db } from './firebase';
+import {collection, addDoc, getDoc, doc, getDocs, deleteDoc} from './firebase';
+import { useState } from 'react';
+
+//계획 생성 함수
+export const createPlan = async (planId) => {
+  try{
+    const title = document.querySelector('input[name="planTitle"]').value;
+    
+    const planData = {
+      planId: planId,
+      title: title,
+    };
+
+    const docRef = await addDoc(collection(db, 'plans'), planData);
+    console.log('계획이 생성되었습니다.', docRef.id);
+  } catch(error){
+    console.error('계획 생성에 실패하였습니다.', error);
+  }
+};
+
+//계획, 노드 매핑 함수
+export const mapNodeToPlan = async (planId) => {
+
+}
+
+
+//노드 수정 함수
+export const updateNode = async (collectionName, documentId, newData) => {
+    try{
+        await collection(collectionName).doc(documentId).update(newData);
+        console.log('데이터 수정에 성공하였습니다.');
+    }catch(error){
+        console.error('데이터 수정을 실패하였습니다.', error);
+    }
+};
+
+//노드 생성 함수
+export const createNode = async () => {
+  try {
+    const title = document.querySelector('input[name="title"]').value;
+    const content = document.querySelector('textarea[name="content"]').value;
+
+    const nodeData = {
+      title: title,
+      content: content,
+    };
+
+    const docRef = await addDoc(collection(db, '노드'), nodeData);
+    const nodeId = docRef.id;
+    console.log('노드가 생성되었습니다.', nodeId);
+  } catch (error) {
+    console.error('노드 생성에 실패하였습니다.', error);
+  }
+};
+
+//노드 삭제 함수
+export const deleteNode = async (nodeId) => {
+    try {
+      // Firestore 컬렉션에서 노드 삭제
+      await deleteDoc(doc(collection(db, '노드'), nodeId));
+      console.log('노드가 삭제되었습니다.', nodeId);
+    } catch (error) {
+      console.error('노드 삭제에 실패하였습니다.', error);
+    }
+  };
+
+
+//모든 노드 정보 출력(콘솔)
+export const printAllNodesConsole = async () => {
+  try {
+    const querySnapshot = await getDocs(collection(db, '노드'));
+    const sortedNodes = querySnapshot.docs
+      .map((doc) => doc.data())
+      .sort((a, b) => a.title.localeCompare(b.title));
+
+    sortedNodes.forEach((nodeData) => {
+      console.log('노드 정보:', nodeData);
+    });
+  } catch (error) {
+    console.error('노드 정보 가져오기에 실패하였습니다.', error);
+  }
+};
+
+
+//모든 노드 정보 출력(front)
+export const printAllNodes = async (setNodes) => {
+  try {
+    const querySnapshot = await getDocs(collection(db,'노드'));
+    const sortedNodes = querySnapshot.docs
+      .map((doc) => {
+        return {...doc.data(), id: doc.id}
+      })
+      .sort((a, b) => a.title.localeCompare(b.title));
+
+    // sortedNodes.forEach((nodeData) => {
+    //   console.log('노드 정보:', nodeData);
+    // });
+
+    console.log('full node :', sortedNodes)
+
+    setNodes(sortedNodes);
+  } catch (error) {
+    console.error('노드 정보 가져오기에 실패하였습니다.', error);
+   }
+};
+
+//모든 노드 삭제(front), 컴포넌트만 삭제한 것임.
+export const clearAllNodes = (setNodes) => {
+  setNodes([]);
+}
+
